5001267ca7add21979564a2f759a678e6a19bd59
ysaiascampos <ysaiascampos@gmail.com>
2021-06-04 21:58:49 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr/>
+    <MiComponte></MiComponte>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MiComponte from './components/MiComponte.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponte
   }
 }
 </script>
---
+++ src/components/MiComponte.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="mi-componente">
+        <h1>Este es mi componente</h1>
+        <h2>{{ subtitulo }}</h2>
+    </div>    
+</template>
+
+<script>
+export default ({
+    name: 'MiComponente',
+    data() {
+        return {
+            subtitulo: "Soy el subtitulo"
+        }
+    }
+})
+</script>
+<style scoped>
+
+</style>
